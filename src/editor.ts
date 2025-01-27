f9661cabd296338fbca8ff93741ffd4a9070e3ee
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2025-01-27 08:52:53 +0100

    fix: expose vscode

--- src/editor.ts
+++ src/editor.ts
@@ -4,12 +4,14 @@ import { ITextFileEditorModel, createModelReference } from 'vscode/monaco'
 import * as monaco from 'monaco-editor'
 import { IReference } from '@codingame/monaco-vscode-editor-service-override'
 import path from 'path'
+import * as vscode from 'vscode'
 
 export class LeanMonacoEditor {
 
   editor: monaco.editor.IStandaloneCodeEditor
   modelRef: IReference<ITextFileEditorModel>
   disposed = false
+  vscodeEnvironment = vscode;
 
   async start(editorEl: HTMLElement, fileName: string, code: string) {
     if (this.disposed) return
